48e6703eb820f48beca22a89574f0c8f3d4047c2
Erika Stigleman <Erika.Stigleman@outlook.com>
2021-03-05 13:13:23 -0500

    Hooks setup Randomizer working

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# New Color Picker Hooks
+Project: Color Picker Hooks
+
+Project Focus: Create an HSL(a) color picker with React Hooks
+
+Objectives
+
+Respond to user events in React
+
+<!-- Use hooks in react to drive changes to a user interface
+Use props to drive inline styles to dynamically update the appearance of DOM elements
+Understand RGB vs HSL color models
+Use controlled form inputs in React -->
+
+Represent the three values, hue, saturation, and lightness with hooks.
+Add three sliders that update their respective values (hue, saturation, and lightness).
+Display the color on the screen, in both text (i.e. hsl(50, 8%, 20%)) and the actual color as a background color on an element.
+Add a button that picks a new random color.
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+function ColorPickerHooks() {
+  const [hue, setHue] = useState(Math.floor(Math.random() * 360))
+
+  const [saturation, setSaturation] = useState(Math.floor(Math.random() * 100))
+
+  const [lightness, setLightness] = useState(Math.floor(Math.random() * 100))
+
+  function randomColor() {
+    setHue(Math.floor(Math.random() * 360))
+    setSaturation(Math.floor(Math.random() * 100))
+    setLightness(Math.floor(Math.random() * 100))
+  }
+
+  document.body.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}% )`
+
+  return (
+    <main>
+      <div>
+        {/* header */}
+        <h1 className="stripe-text>">Color Pickers</h1>
+
+        {/* add sliders */}
+        <section>
+          <label>
+            <h2>
+              <div className="rainbow-letters">
+                <span>H</span>
+                <span>U</span>
+                <span>E</span>
+              </div>
+              <input
+                name="Hue"
+                type="range"
+                value={hue}
+                min="0"
+                max="360"
+                onChange={(event) => setHue(event.target.value)}
+              />
+            </h2>
+          </label>
+
+          <label>
+            <h2>
+              <div className="fadingColor">
+                <span>S</span>
+                <span>A</span>
+                <span>T</span>
+                <span>U</span>
+                <span>R</span>
+                <span>A</span>
+                <span>T</span>
+                <span>I</span>
+                <span>O</span>
+                <span>N</span>
+              </div>
+              <input
+                name="Saturation"
+                type="range"
+                value={saturation}
+                min="0"
+                max="100"
+                onChange={(event) => setSaturation(event.target.value)}
+              />
+            </h2>
+          </label>
+          <h2>
+            <div className="brightLetter">
+              <span>L</span>
+              <span>I</span>
+              <span>G</span>
+              <span>H</span>
+              <span>T</span>
+              <span>N</span>
+              <span>E</span>
+              <span>S</span>
+              <span>S</span>
+            </div>
+            <input
+              name="Lightness"
+              type="range"
+              value={lightness}
+              min="0"
+              max="100"
+              onChange={(event) => setLightness(event.target.value)}
+            />
+          </h2>
+        </section>
+      </div>
+      <footer>
+        <button onClick={randomColor}> Pick A Random Color</button>
+      </footer>
+    </main>
+  )
+}
 
 export function App() {
-  return <div>Hello, World!</div>
+  return <ColorPickerHooks />
 }
